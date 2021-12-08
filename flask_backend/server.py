f45dfea39f5d82e217889e732aec5b19fcf0c21d
thuanpv1 <thuanpv@egt.vn>
2021-12-08 08:22:55 +0700

    fix bugs

--- flask_backend/server.py
+++ flask_backend/server.py
@@ -57,14 +57,46 @@ def read_DICOM_slices(path, viewOfDicomName, viewOfDicomSliceNumber, viewOfDicom
     img_shape = list(imgPixelData.shape)
     img_shape.append(len(slices))
     img3d = np.zeros(img_shape)
-    
+    isSliceLocation = False
+    try:
+        slices.sort(key=lambda x: (x.SliceLocation))
+        isSliceLocation = True
+    except:
+        slices.sort(key=lambda x: int(x.InstanceNumber))
     # Fill 3D array with the images from the files
     for i, img2d in enumerate(slices):
         img3d[:, :, i] = img2d.pixel_array
 
+    columns = ['PatientID', 'PatientName', 'StudyDescription', 'PatientBirthDate', 'StudyDate', 'Modality', 'Manufacturer', 'InstitutionName', 'ProtocolName']
+    col_dict = {col: [] for col in columns}
+
+    try:
+        for col in columns: 
+            col_dict[col].append(str(getattr(files[0], col)))
+        
+        df = pd.DataFrame(col_dict).T
+        df.columns = ['Patient']
+    except:
+        df = pd.DataFrame([])
+
     array = None
+    if viewOfDicomSliceNumber is None:
+        viewOfDicomSliceNumber = 0
+    if viewOfDicomThreshold is None:
+        viewOfDicomThreshold = 0
+    if viewOfDicomName is None:
+        viewOfDicomName = 'Axial'
+    
+    threshold = int(img3d[:, :, viewOfDicomSliceNumber].max())
+    
     if (viewOfDicomName == 'Axial'):
         array = normalize_image(filter_image(int(viewOfDicomThreshold), img3d[:, :, int(viewOfDicomSliceNumber)]))
+    if (viewOfDicomName == 'Coronal') and isSliceLocation:
+        threshold = int(img3d[viewOfDicomSliceNumber, :, :].max())
+        array = normalize_image(filter_image(int(viewOfDicomThreshold), resize(ndimage.rotate(img3d[int(viewOfDicomSliceNumber), :, :].T, 180), (img3d.shape[0],img3d.shape[0]))))
+    if (viewOfDicomName == 'Sagittal') and isSliceLocation:
+        threshold = int(img3d[:, viewOfDicomSliceNumber, :].max())
+        array = normalize_image(filter_image(int(viewOfDicomThreshold), resize(ndimage.rotate(img3d[:, int(viewOfDicomSliceNumber), :], 90), (img3d.shape[0],img3d.shape[0]))))
 
     array *= 255
     data = im.fromarray(array)
@@ -81,10 +113,16 @@ def read_DICOM_slices(path, viewOfDicomName, viewOfDicomSliceNumber, viewOfDicom
         png_encoded = base64.b64encode(data)
 
     os.remove(imageName)
-    return png_encoded
+    return json.dumps({
+            'patientInfo': df.to_dict(),
+            'isSliceLocation': isSliceLocation,
+            'threshold': threshold,
+            'numberOfSlices': len(slices),
+            'base64Img': (png_encoded).decode()
+        })
 
 
-@app.route("/")
+@app.route("/", methods = ['GET'])
 def read_dicom():
     folderId = request.args.get('folderId')
     viewOfDicomName = request.args.get('viewOfDicomName')
@@ -94,7 +132,13 @@ def read_dicom():
     print('viewOfDicomName===', viewOfDicomName)
     print('viewOfDicomSliceNumber===', viewOfDicomSliceNumber)
     print('viewOfDicomThreshold===', viewOfDicomThreshold)
+    if folderId is None:
+        folderId = 'test'
     return read_DICOM_slices("./DCOM/" + folderId + "/", viewOfDicomName, viewOfDicomSliceNumber, viewOfDicomThreshold)
+@app.route("/getfolder", methods = ['GET'])
+def read_folders():
+    my_list = os.listdir('./DCOM')
+    return json.dumps(my_list)
 
 if __name__=="__main__":
     app.run(debug=True)
